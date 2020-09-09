c8bb5694e6f4e827c4558efca5f9e6d1cf624fe2
D0KKAEl3I <dldudtjr850@gmail.com>
2020-09-09 22:31:57 +0900

    메인페이지 애니메이션 완성

--- static/design_js/mainpage.js
+++ static/design_js/mainpage.js
@@ -1,53 +1,58 @@
 toggle = true;
 function clicking(char) {
-    console.log(toggle)
+    var letters = ['c', 'h', 'a', 't', 'e', 'r', 's'];
     if(toggle){
-        var letters = ['C', 'H', 'A', 'T', 'E', 'R', 'S'];
         for(var i in letters){
             if(letters[i] == char) {
-                document.getElementById(letters[i]).disabled = true;
                 continue;
             }
-            dels=0.0
-            document.getElementById(letters[i]).animate([
+            var btn=document.getElementById(letters[i]);
+            btn.animate([
                 {transform: 'translate(0, 3500px)'}
             ], {
                 duration:1500,
                 fill:"forwards"
             })
-            // document.getElementById(letters[i]).style.animation=`intro 1.3s ${dels}s cubic-bezier(0.0, 0.0, 0.58, 1.0) reverse forwards`;
-            dels += 0.1;
         }
         var tar = document.getElementById(char)
         var tarLeft = tar.getBoundingClientRect().left;
         var abLeft = window.pageXOffset + tarLeft
-        // var wi = 
         var wi = (screen.width/2)-(document.getElementById(char).clientWidth*0.5)-abLeft
-        console.log(wi)
-        console.log(abLeft)
         document.getElementById(char).animate([
             {transform: `translate( ${wi}px, 0)`+" "+`scale(2.5, 2.5)`}
         ], {
             duration:600,
-            easing:"cubic-bezier(0.0, 0.0, 0.3, 2)",
+            easing:"cubic-bezier(0.0, 0.0, 0.2, 1)",
             delay:600,
             fill:"forwards"
         })
         toggle = !toggle        
     } else {
-        var letters = ['C', 'H', 'A', 'T', 'E', 'R', 'S'];
+        var tar = document.getElementById(char)
+        var tarLeft = tar.getBoundingClientRect().left;
+        var abLeft = window.pageXOffset + tarLeft
+        var wi = (screen.width/2)-(document.getElementById(char).clientWidth*0.5)-abLeft
+        document.getElementById(char).animate([
+            {transform: `translate( 0, 0)`+" "+`scale(1, 1)`}
+        ], {
+            duration:600,
+            easing:"cubic-bezier(0.0, 0.0, 0.2, 1)",
+            fill:"forwards"
+        })
+        
         for(var i in letters){
             if(letters[i] == char) {
                 continue;
-            } 
-            // document.getElementById(letters[i]).style.animation='intro 1.3s cubic-bezier(0.0, 0.0, 0.58, 1.0) forwards';
-            document.getElementById(letters[i]).animate([
+            }         
+            var btn=document.getElementById(letters[i]);
+            btn.animate([
                 {transform: 'translate(0, 0)'}
             ], {
                 duration:1500,
                 fill:"forwards",
-                easing:"cubic-bezier(0.0, 0.0, 0.58, 1.0)"
+                easing:"cubic-bezier(0.0, 0.0, 0.2, 1.0)"
             })
+            
         }
         toggle = !toggle;
     }
