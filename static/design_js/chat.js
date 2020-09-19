3507a09a78f169ff8d6df85c0723ab0f3e883788
D0KKAEl3I <dldudtjr850@gmail.com>
2020-09-19 13:56:18 +0900

    chat.html 방만들기 페이지 토글기능 추가중

---
+++ static/design_js/chat.js
@@ -0,0 +1,8 @@
+var filtertoggle
+function filtering(num){
+    if(num == 0){
+        document.getElementById("filter2").style.display='inline-block';
+    } else if(num == 1) {
+        document.getElementById("filter2").style.display='inline-block';
+    } 
+}
